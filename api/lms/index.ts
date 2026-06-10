a78f814ba228cb591c515b78ebb2bd721398310d
Paulo Cidro <paulocidro@gmail.com>
2026-06-10 17:04:39 -0300

    adding course post and lessons post

--- api/lms/index.ts
+++ api/lms/index.ts
@@ -1,8 +1,67 @@
 import { Api } from "../../core/utils/abstract.ts";
+import { RouteError } from "../../core/utils/route-error.ts";
 import { lmsTables } from "./tables.ts";
 
 export class LmsApi extends Api {
+  handlers = {
+    postCourses: (req, res) => {
+      const { slug, title, description, lessons, hours } = req.body;
+      const writeResult = this.db
+        .query(
+          /*sql*/
+          `
+        INSERT OR IGNORE INTO "courses"("slug", "title", "description", "lessons", "hours") VALUES(?,?,?,?,?)
+        `,
+        )
+        .run(slug, title, description, lessons, hours);
+      console.log(writeResult);
+      if (writeResult.changes === 0) {
+        throw new RouteError(400, "erro ao criar curso");
+      }
+      res.status(201).json({
+        id: writeResult.lastInsertRowid,
+        changes: writeResult.changes,
+        title: "Curso criado!",
+      });
+    },
+    postLessons: (req, res) => {
+      const {
+        courseSlug,
+        slug,
+        title,
+        seconds,
+        video,
+        description,
+        order,
+        free,
+      } = req.body;
+      const writeResult = this.db
+        .query(
+          /*sql*/
+          `
+        INSERT OR IGNORE INTO "lessons"("course_id", "slug", "title", "seconds", "video", "description", "order", "free") VALUES((SELECT "id" FROM "courses" WHERE "slug" = ?),?,?,?,?,?,?,?)
+        `,
+        )
+
+        .run(courseSlug, slug, title, seconds, video, description, order, free);
+      console.log(writeResult);
+      if (writeResult.changes === 0) {
+        throw new RouteError(400, "erro ao criar aula");
+      }
+      res.status(201).json({
+        id: writeResult.lastInsertRowid,
+        changes: writeResult.changes,
+        title: "Aula criada!",
+      });
+    },
+  } satisfies Api["handlers"];
+
   tables(): void {
     this.db.exec(lmsTables);
+  }
+
+  routes(): void {
+    this.router.post("/lms/courses", this.handlers.postCourses);
+    this.router.post("/lms/lessons", this.handlers.postLessons);
   }
 }
